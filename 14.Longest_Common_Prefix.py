07cf64050907a127c53206cbc753361e5afdef3d
thomas-hunt4 <thomas.a.hunt.4@gmail.com>
2025-05-28 19:45:46 +0200

    5.28.25 leetcode with Drashti

--- 14.Longest_Common_Prefix.py
+++ 14.Longest_Common_Prefix.py
@@ -24,3 +24,25 @@ strs[i] consists of only lowercase English letters if it is non-empty.
 """
 
 def longestCommonPrefix(self, strs: List[str]) -> str:
+    #create list to store values of prefixes from string []
+    
+    
+    
+    #loop/iterate through list items strings
+        #check index 0 and 1 of each list item
+        #save index 0 and 1 of each list item to created list storage as string
+
+    #for prefixes stored in created list
+            #compare strings in stored list to find most common
+            #return the most common prefix
+    
+    #handle edge case
+        #if no common prefix return empty string
+        #reorder logic if needed
+    
+        
+    
+    
+
+    
+        
